e3f6c48655e7932b16fe1c039061e24aab39f202
Hitender97 <pahujahitender@gmail.com>
2024-09-24 16:05:01 +0530

    Added float data type support and check loop for non positive ints.

--- basic_calculator.py
+++ basic_calculator.py
@@ -3,23 +3,30 @@ import os
 
 def add(num1,num2):
     return num1+num2
-def multI(num1,num2):
+def multi(num1,num2):
     return num1*num2
 def sub(num1,num2):
     return num1-num2
 def div(num1,num2):
-    return num1/num2
+    return (num1/num2)
+def is_positive(num):
+    return num > 0
+        
 
-a=int(sys.argv[1])
+a=float(sys.argv[1])
 operation = sys.argv[2]
-b=int(sys.argv[3])
+b=float(sys.argv[3])
+
+if not (is_positive(a) and is_positive(b)):
+    print("Both numbers must be positive.")
+    sys.exit(1)
 
 if operation == "add":
     print(add(a,b))
 elif operation == "sub":
     print(sub(a,b))
-elif operation == "multI":
-    print(multI(a,b))
+elif operation == "multi":
+    print(multi(a,b))
 elif operation == "div":
     print(div(a,b))
 else:
